52a00587dcd39b770a5b17e1b5539334d35c750f
Docbsk <108417086+Alby9z@users.noreply.github.com>
2024-05-23 13:36:33 +0200

    commit-4

--- js/acceuil.js
+++ js/acceuil.js
@@ -6,3 +6,33 @@ document.addEventListener("DOMContentLoaded", function () {
     navMenu.classList.toggle("active");
   });
 });
+
+// Sélectionnez l'élément à animer
+const element = document.querySelector(".texte-acceuil");
+
+// Fonction pour vérifier si un élément est dans la vue
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Fonction pour ajouter la classe d'animation lorsque l'élément est visible
+function addAnimationClass() {
+  if (isElementInViewport(element)) {
+    element.classList.add("slideInDown-active");
+    // Supprimer l'écouteur d'événement une fois l'animation déclenchée
+    window.removeEventListener("scroll", addAnimationClass);
+  }
+}
+
+// Écouteur d'événement pour déclencher l'animation lors du scroll
+window.addEventListener("scroll", addAnimationClass);
+
+// Appel initial pour vérifier si l'élément est déjà visible au chargement de la page
+addAnimationClass();

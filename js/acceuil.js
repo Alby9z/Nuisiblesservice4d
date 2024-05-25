129ecb757f609c8743aa72bb5e10e3a054e700b2
Docbsk <108417086+Alby9z@users.noreply.github.com>
2024-05-25 05:16:37 +0200

    ajout_icon1

--- js/acceuil.js
+++ js/acceuil.js
@@ -6,3 +6,32 @@ document.addEventListener("DOMContentLoaded", function () {
     navMenu.classList.toggle("active");
   });
 });
+
+$(document).ready(function () {
+  $("#icon_barre").select2({
+    templateResult: function (option) {
+      if (!option.id) {
+        return option.text;
+      }
+
+      var imgSrc = $(option.element).data("img-src");
+      if (!imgSrc) {
+        return option.text;
+      }
+
+      var $img = $("<img>", {
+        class: "img-option",
+        src: imgSrc,
+        height: "20px",
+        style: "margin-right: 10px;",
+      });
+
+      var $span = $("<span>", {
+        text: option.text,
+      });
+
+      var $div = $("<div>").append($img).append($span);
+      return $div;
+    },
+  });
+});

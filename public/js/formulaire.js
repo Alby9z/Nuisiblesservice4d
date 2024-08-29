5ed562705b60c0a7f851217cc8d16db143ab2bb6
Xaluks <165258639+Xaluks@users.noreply.github.com>
2024-08-29 19:42:47 +0200

    dfg

--- public/js/formulaire.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit",function(e) {
-    e.preventDefault(),e=new FormData(this),fetch("send_email.php", {
-        method:"POST",body:e
-    })
-    .then(e=>e.text()).then(e=> {
-        document.getElementById("confirmationMessage").style.display="block",document.getElementById("contactForm").reset()
-    })
-    .catch(e=>console.error("Erreur:",e))
-});

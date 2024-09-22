0165c12c1fa3ec7c3cf09dd7a9f4c93a1ea3274a
Docbsk <justineetbrian90@gmail.com>
2024-09-22 12:17:50 +0200

    ghg

--- public/js/produit.js
+++ public/js/produit.js
@@ -1,16 +1,2 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let summaries = document.querySelectorAll("summary");
-    summaries.forEach((summary) => {
-        let details = summary.parentElement;
-        details.removeAttribute("open");
-        summary.addEventListener("click", function(e) {
-            e.preventDefault(); 
-            if (!details.hasAttribute("open")) {
-                details.setAttribute("open", "true");  
-            } else {
-                details.removeAttribute("open"); 
-            }
-        });
-    });
-});
+document.addEventListener("DOMContentLoaded", function() {let summaries = document.querySelectorAll("summary");summaries.forEach((summary) => {let details = summary.parentElement;details.removeAttribute("open");summary.addEventListener("click", function(e) {e.preventDefault(); if (!details.hasAttribute("open")) {details.setAttribute("open", "true");} else {details.removeAttribute("open");}});});});
 
